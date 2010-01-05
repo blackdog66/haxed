aadf61e7d8dcd24bd6160161b95aab722c44c99f
Daniel Grad <dg@online-business.ro>
2010-01-05 19:00:48 +0200

    add derives-from key

--- src/tools/haxelib/Parser.hx
+++ src/tools/haxelib/Parser.hx
@@ -246,7 +246,8 @@ class Parser {
       .add("description",true)
       .add("comments",true)
       .add("tags",true,Validate.toArray)
-      .add("license",true,function(v) { return v.toUpperCase() ;} );
+      .add("license",true,function(v) { return v.toUpperCase() ;})
+      .add("derives-from",false,Validate.toArray );
     
     Validate.forSection(Config.BUILD)
       .add("depends",false,Validate.depends)
