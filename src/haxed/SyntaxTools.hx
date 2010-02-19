e69a095085ad4d6a6ce502fa6b82f58452f3b1f2
blackdog <blackdog@blackdog.(none)>
2010-02-18 23:16:24 -0300

    add SyntaxTools.hx

--- src/haxed/SyntaxTools.hx
+++ src/haxed/SyntaxTools.hx
@@ -1,7 +1,10 @@
 package haxed;
 
+import Type;
 import haxed.Os;
 import haxed.Reader;
+
+using StringTools;
 
 typedef Info = { line:Int,col:Int};
 
@@ -12,8 +15,19 @@ typedef Token<T> = {
 	var converter:Converter<T>;
 }
 
-typedef IToken<T> = { > Token<T>,
-	var pushback:Int;
+private typedef IToken<T> = {> Token<T> ,
+  var pushback:Int;
+  var discard:Bool;
+}
+
+enum SynOptions {
+  Pushback(i:Int);
+  Discard;
+}
+
+enum SynStyle {
+  LINE;
+  CHUNK;
 }
 
 class Tokenizer<T> {
@@ -21,10 +35,12 @@ class Tokenizer<T> {
   static var DEFAULT_CHUNK_SIZE = 512;
   
   var reader:Reader;
+  var style:SynStyle;
   var curChar:Int;
   var lineNo:Int;
   var lineStart:Int;
-  var reWnd:Bool;
+  var lineLen:Int;
+  var startCh:Int;
   var curLine:StringBuf;
   var recognisedTokens:Array<IToken<T>>;
   var eof:Bool;
@@ -32,30 +48,44 @@ class Tokenizer<T> {
   var leftOver:String;
   var chunker:Void->String;
   var charCodeAt:Int->Int;
+  var inChunk:Int;
   
-  public function new(rd:Reader,useChunker=true) {
+  public function new(rd:Reader,?s:SynStyle) {
     recognisedTokens = new Array();
     reader = rd;
     curChar = 0;
-    lineNo = 1;
-    reWnd = false;
+    lineNo = 0;
     lineStart = 0;
+    lineLen = 0;
+    startCh = 0;
     eof = false;
     curLine = new StringBuf();
     leftOver = "";
+    style = (s == null) ? CHUNK : s;
 
-    if (rd.canChunk() && useChunker)
+    if (rd.canChunk() && style == CHUNK)
       chunker = reader.nextChunk;
     else {
-      chunker = nextChunk;
+      chunker = lineChunk;
     }
       
     charCodeAt = reader.charCodeAt;
   }
 
   public function
-  add(rec:EReg,pb:Int=0,conv:Converter<T>) {
-    recognisedTokens.push({recogniser:rec,converter:conv,pushback:pb});
+  match(re:EReg,conv:Converter<T>,?options:Dynamic) {
+    var matcher = {recogniser:re,converter:conv,pushback:0,discard:false};
+    if (options != null) {
+      var opts:Array<SynOptions> = (!Std.is(options,Array)) ? [options] : options;
+      for (o in opts) {
+        switch(o) {
+        case Pushback(pb): matcher.pushback = pb;
+        case Discard: matcher.discard = true;
+        }
+      }
+    
+    }
+    recognisedTokens.push(matcher);
     return this;
   }
   
@@ -81,7 +111,7 @@ class Tokenizer<T> {
   
   public inline function
   column() {
-    return (curChar-1) - lineStart;
+    return fromBOL()+inChunk;
   }
 
   public inline function
@@ -95,46 +125,49 @@ class Tokenizer<T> {
   
   public function
   nextChar() {
-    var nc = charCodeAt(curChar);    
-
-    if (nc == EOF) {
-      return EOF;
-    }
- 
-    //    if (!reWnd) curLine.add(nc);
+    var nc = charCodeAt(curChar);
     
-    if (isNL(nc) && ! reWnd) {
-     
+    if (isNL(nc)) {
+        
 #if debug
-      Os.print(">>"+":"+lineNo+"("+curChar+"): "+curLine.toString()+"<");
+        Os.print(">>"+":"+lineNo+"("+curChar+"): "+curLine.toString()+"<");
+        curLine = new StringBuf();
 #end
-      // curLine = new StringBuf();
-      lineStart = curChar + 1;
-      lineNo++;
+        lineStart = curChar + 1;
+        lineNo++;
+    } else {      
+#if debug
+      if (nc != EOF)
+        curLine.addChar(nc);
+#end
     }
     
     curChar++;
-    //reWnd = false;
     return nc;
   }
 
-  function nextChunk() {
+  
+  function lineChunk() {
     var
-      sb = new StringBuf(), 
-      c,
-      i = 0;
+      sb = new StringBuf(), c;
+    
     while ((c = nextChar()) != -1) {
       sb.addChar(c);
-      if (i++ > DEFAULT_CHUNK_SIZE) break;
+      if (c == 10) {
+        break;
+      }
     }
 
-    return sb.toString();
+    var s = sb.toString();
+    lineLen = s.length;
+    return s;
   }
 
   function withChunks(chunk:String,fn:String->Int) {
 
-    if(chunk == "")
+    if(chunk == "") {
       chunk = chunker();
+    }
     
     do {
       var np = fn(chunk);
@@ -144,10 +177,14 @@ class Tokenizer<T> {
         if (atEof()) {          
           break;
         }
-        
-        chunk += chunker();        
+
+        if (style == LINE) {
+          chunk = chunker(); // no match, get new line
+        } else
+          chunk += chunker(); // extend chunk until match
 
       default:
+        startCh = chunk.length;
         chunk = chunk.substr(np);
         break;
       }
@@ -156,31 +193,28 @@ class Tokenizer<T> {
   }
 
   public function
+  fromBOL() {
+    return lineLen - startCh;
+  }
+  
+  public function
   nextToken() {
     var me =  this;
     var tok = null;
-
+      
     leftOver = withChunks(leftOver,function(chunk) {
         for (rt in me.recognisedTokens) {
           if (rt.recogniser.match(chunk)) {
-            var p = rt.recogniser.matchedPos();
-            #if debug
-            try {
-            #end
-              tok = rt.converter(rt.recogniser);
-            #if debug
-            } catch(ex:Dynamic) {
-              trace("converter failed with "+rt.recogniser.matched(0) +" in context "+chunk);
-              trace(ex);
-              Os.exit(1);
+            var p = rt.recogniser.matchedPos();            
+            tok = rt.converter(rt.recogniser);
+            if (tok != null) {
+              me.inChunk = p.pos;
+              return (rt.discard) ? chunk.length : p.pos + p.len - rt.pushback;
             }
-            #end
-            return p.pos + p.len - rt.pushback;
           }
-        }        
+        }
         return -1;
       });
-    //        trace(tok);    
     return tok;
   }
 
@@ -197,12 +231,6 @@ class Tokenizer<T> {
       }
     }
   }
-  
-  public function
-  rewind() {
-    reWnd = true;
-    curChar--;
-  }
 
   public inline function
   prevChar() {
@@ -215,34 +243,44 @@ class Tokenizer<T> {
     if (tok != expected) throw "expected:" + expected + " got "+tok;
     return tok;
   }
-
   
   public function
   syntax(msg:String) {
-    if (column() > -1)
-      Os.print("At line "+lineNo+" col "+column()+": "+msg);
-    else 
-      Os.print("At line "+ (lineNo -1) +": "+msg);
-
+    Os.print("At line "+lineNo+" col "+column()+": "+msg);
     Os.exit(1);
   }
 }
 
-typedef Action = Dynamic->Void;
+enum CallType {
+  ByName;
+  ByArray;
+  ByEnum;
+}
+
+enum ActionDef<S> {
+  ONTRAN(s:S,e:Dynamic,f:Dynamic);
+  ONSTATE(e:Dynamic,f:Dynamic);
+}
+
+typedef Action = {fn:Dynamic,type:CallType};
 
 class Parser<S,E> {
-  var funcs:Hash<Action>;
+  var actions:Hash<Action>;
   var trans:Hash<S>;
   var curState:S;
-  var retState:S;
   var startState:S;
   var tokenizer:Tokenizer<E>;
+  var allowed:Hash<Bool>;
+  var eventStr:E->String;
+  var stack:Array<E>;
   
   public function new(ss:S,t:Tokenizer<E>) {
-    funcs = new Hash<Action>();
+    actions = new Hash<Action>();
     trans = new Hash<S>();
-    startState = curState = retState = ss;
+    allowed = new Hash<Bool>();
+    startState = curState = ss;
     tokenizer = t;
+    stack = new Array<E>();
   }
 
   public inline function
@@ -250,47 +288,161 @@ class Parser<S,E> {
     return curState;
   }
 
-  inline function actionID(s:S,?e:E) {
-    var s = Type.enumConstructor(s);
-    if (e != null) {
-      s += Type.enumConstructor(e);
-    }
-    return s;
+  inline function
+  getArray(e:Dynamic):Array<E> {
+    return (!Std.is(e,Array)) ? [e] : e;
   }
 
-  public function
-  on(s:S,f:Action) {
-    funcs.set(actionID(s),f);
-    return this;
-  }
-
-  public function
-  tr(s:S,e:E,newState:S) {
-    trans.set(actionID(s,e),newState);
-    return this;
+  inline function
+  stateID(s:S) {
+    return Type.enumConstructor(s);
   }
   
-  public function
-  parse(context:Dynamic) {
-    var t;
-    while((t = nextToken()) != null)
-      execute(t,context);
-    return context;
-  }
-  
-  public
-  function execute(event:E,context:Dynamic) {    
-    var f = funcs.get(actionID(curState,event));
-    if (f != null)
-      f(context);    
+  inline function
+  eventID(e:E) {
+    return Type.enumConstructor(e);
   }
 
-    
+  inline function
+  transID(s:S,e:Dynamic):String {
+    return stateID(s) + "-" + eventID(e);
+  }
+
   public inline function
-  nextToken() {
+  syntax(m) {
+    tokenizer.syntax(m);
+  }
+
+  public inline function
+  push(e:E) {
+    stack.push(e);
+  }
+
+  public inline function
+  pop():E {
+    return stack.pop();
+  }
+                             
+  public function
+  nextToken():E {
+    if (stack.length > 0) return stack.pop();
     return tokenizer.nextToken();
   }
+  
+  function
+  onTransition(s:S,e:Dynamic,f:Dynamic) {
+    if (!Reflect.isFunction(f))
+      throw "3rd param should be a function";
 
+    for (ev in getArray(e)) {
+      var tID = transID(s,ev);
+      if (actions.exists(tID)) Os.print("Warning: Overwriting "+tID);
+      actions.set(tID,{fn:f,type:(Std.is(e,Array)) ? ByEnum : ByName});
+    }
+    
+    return this;
+  }
+
+  function
+  onEvent(e:Dynamic,f:Dynamic) {
+    if (!Reflect.isFunction(f))
+      throw "2nd param should be a function";
+
+    for (ev in getArray(e)) {
+      var sID = stateID(e);
+      if (actions.exists(sID)) Os.print("Warning: Overwriting event "+sID);
+      actions.set(sID,{fn:f,type:(Std.is(e,Array)) ? ByEnum : ByName});
+    }
+    
+    return this;
+  }
+  
+
+  public function
+  define(trans:Array<ActionDef<S>>) {
+    for (t in trans) {
+      switch(t) {
+      case ONTRAN(s,e,f): onTransition(s,e,f);
+      case ONSTATE(e,f): onEvent(e,f);
+      }
+    }
+    return this;
+  }
+
+  function callFn(call:Action,event:Dynamic):S {
+    return switch(call.type) {
+    case ByEnum:
+      call.fn(event);
+    case ByName:
+      Reflect.callMethod(this,call.fn,Type.enumParameters(event));
+    case ByArray:
+      call.fn(Type.enumParameters(event));
+    }
+  }
+  
+  public function
+  parse() {
+    var
+      event:E,
+      sID = stateID(curState);
+    while((event = nextToken()) != null) {
+
+      var
+        eID = eventID(event),
+        action = actions.get(sID+"-"+eID); // a transition
+      
+	  #if TRACESTATES
+      var lastState = curState;
+      #end
+      
+      if (action != null) {
+        curState = callFn(action,event);
+        sID = stateID(curState);
+
+        #if TRACESTATES
+        trace(">> "+stateID(lastState)+" --> "+event+" --> "+curState);
+        #end
+        
+        action = actions.get(sID) ; // a state event
+        if (action != null) {
+
+          #if TRACESTATE
+          trace(">> executing event "+curState);
+          #end
+          
+          curState = callFn(action,curState);
+          sID = stateID(curState);
+          
+        }
+        
+      } else {
+        if (allowed.exists(eID)) {
+          #if TRACESTATES
+          trace(">> skipping:"+transID(curState,event)+", state remains:"+curState);
+          #end
+        } else  {
+          var expected = [];
+          
+          for (k in actions.keys()){
+            if (k.startsWith(sID)) {
+              expected.push(k.substr(k.indexOf("-")+1));
+            }
+          }
+
+          var unexpected = (eventStr != null) ? eventStr(event) : eID;
+          syntax("!! Unexpected "+unexpected+", expected "+expected.join(","));
+        }
+      }
+    }
+  }
+
+  public function
+  allow(e:Dynamic) {
+    for (flt in getArray(e))
+      allowed.set(Type.enumConstructor(flt),true);
+    return this;
+  }
+  
   public function
   readAssert(expected:E) {
     var tok = nextToken();
@@ -299,24 +451,10 @@ class Parser<S,E> {
   }
 
   public function
-  nextState(newState:S,?rs:S,rw=false) {
-    if (rs != null)
-        retState = rs;
-
-    curState = newState;
-    //if (rw) rewind();
+  tokenString(f:E->String) {
+    eventStr = f;
   }
 
-  public function
-  popState() {
-    if (retState == null || retState == curState /* guard against infinite loops */)
-      curState = startState;
-    else 
-      curState = retState;
-    
-    retState = null;
-    
-  }
 }
 
 
